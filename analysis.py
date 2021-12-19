6d92ad9bdaa40601c6dc1511c3c9a79f4c642b6b
Nuelam9 <Nuelam9@github.com>
2021-12-19 16:38:07 +0100

    Final wavelen_denoising func

--- analysis.py
+++ analysis.py
@@ -1,4 +1,3 @@
-import numpy as np 
 import matplotlib.pyplot as plt
 import pywt
 
@@ -15,11 +14,14 @@ def q2(x):
     return x.quantile(0.975)
 
 def wavelet_denoising(df, waveletname='sym4'):
-    """[summary]
+    """PLot of wavelen data and coefficients approximation.
 
     Args:
         df ([DataFrame]): [description].
         waveletname (str): ['coif5', 'sym4', 'sym5'].
+
+    Returns:
+        [tuple[array, array]]: wavelen data and coefficients.
     """
     t = df.Seconds.to_numpy() / (15*60)
     data = df.Load.to_numpy().copy()
@@ -28,21 +30,21 @@ def wavelet_denoising(df, waveletname='sym4'):
     plt.show()
 
     levels = pywt.dwt_max_level(len(data), waveletname)
-    dec=2
-    fig, axarr = plt.subplots(nrows=levels, ncols=2, figsize=(20,20))
+    data_l, c_l = pywt.dwt(data, waveletname)
+    dec = 2.
+    fig, axarr = plt.subplots(nrows=levels, ncols=2, figsize=(20, 20))
     for i in range(levels):
-        (globals()[f'data_l{i + 1}d'],
-         globals()[f'c_l{i + 1}d']) = pywt.dwt(data, waveletname)
-        axarr[i, 0].plot(globals()[f'data_l{i + 1}d'], 'r')
+        axarr[i, 0].plot(data_l, 'r')
         axarr[i, 0].set_xlim(t[0] // dec, t[-1] // dec)
-        axarr[i, 1].plot(globals()[f'c_l{i + 1}d'], 'g')
+        axarr[i, 1].plot(c_l, 'g')
         axarr[i, 1].set_xlim(t[0] // dec, t[-1] // dec)
         axarr[i, 0].set_ylabel(f"Level {i + 1}", fontsize=14, rotation=90)
         #axarr[i, 0].set_yticklabels([])
-        dec *= 2
+        dec *= 2.
         if i == 0:
             axarr[i, 0].set_title("Approximation coefficients", fontsize=14)        
             axarr[i, 1].set_title("Detail coefficients", fontsize=14)        
         #axarr[i, 1].set_yticklabels([])
     plt.tight_layout()
     plt.show()
+    return data_l, c_l
